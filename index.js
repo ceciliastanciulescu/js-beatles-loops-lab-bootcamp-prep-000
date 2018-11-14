5241fd5c0a03ba6ce6a1ebeb3fc7bc54cb3b5d41
Stanciulescu Cecilia Ionela <cecilia.zagrean@gmail.com>
2018-11-14 23:05:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for(var i = 0; i < musicians.length; i++){
+    arr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  var arr = [];
+  while(i < facts.length){
+    arr.push(facts[i] + "!!!");
+    i = i + 1;
+  }
+  return arr;
+}
+
+function iLoveTheBeatles(num){
+  var arr = [];
+  
+}
